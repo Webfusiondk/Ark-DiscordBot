56b4bf344c5f31834e428d914a05af928d18f3f0
Marc Andersen <marchot1999@gmail.com>
2020-09-29 13:59:45 +0200

    Json.cs Added

--- Ark-DiscordBot/Bot.cs
+++ Ark-DiscordBot/Bot.cs
@@ -10,11 +10,13 @@ using Newtonsoft.Json;
 using System.Collections.ObjectModel;
 using DSharpPlus.Interactivity;
 using DSharpPlus.Entities;
+using System.Linq;
 
 namespace Ark_DiscordBot
 {
     class Bot
     {
+        Json j = new Json();
         List<Member> listOfMembers = new List<Member>();
         public InteractivityModule Interactivity { get; private set; }
         public DiscordClient Client { get; private set; }
@@ -62,18 +64,84 @@ namespace Ark_DiscordBot
             Commands.RegisterCommands<TaskCommand>();
             Commands.RegisterCommands<MemberCommand>();
             await Client.ConnectAsync();
+            AddMembersToList(j.ReadMembers());
             GetAllMembers();
-                        
+            AddNewRole();
+            NewMemberJoin();
+            RoleUpdate();
+
             await Task.Delay(-1);
         }
-        private async Task NewMemberJoin()
+        private void RoleUpdate()
+        {
+            Client.GuildMemberUpdated += Client_GuildMemberUpdated;
+        }
+
+        private Task Client_GuildMemberUpdated(GuildMemberUpdateEventArgs e)
+        {
+            e.Guild.CurrentMember.Roles;
+            if (CheckForNewUser(e.Guild.CurrentMember.Id) == true)
+            {
+                for (int i = 0; i < listOfMembers.Count; i++)
+                {
+                    if (listOfMembers[i].MemberID == e.Guild.CurrentMember.Id)
+                    {
+                        listOfMembers.RemoveAt(i);
+                        listOfMembers.Add(new Member(e.Guild.CurrentMember.DisplayName,e.Guild.CurrentMember.Id,e.Guild.CurrentMember.Roles));
+                    }
+                }
+            }
+           j.UpdateMembers(listOfMembers);
+            return Task.CompletedTask;
+        }
+
+        private void AddNewRole()
+        {
+            Client.GuildAvailable += NewRole;
+        }
+
+        private Task NewRole(GuildCreateEventArgs e)
+        {
+            bool isCreated = false;
+            var temp = e.Guild.Roles;
+            foreach (var item in temp)
+            {
+                if (item.Name == "Member")
+                {
+                    isCreated = true;
+                }
+                Console.WriteLine(item.Id);
+            }
+            if (isCreated == false)
+            {
+                e.Guild.CreateRoleAsync("Member", Permissions.None, DiscordColor.SapGreen, true, true, "New Member");
+            }
+            return Task.CompletedTask;
+        }
+        private void AddMembersToList(List<Member> list)
+        {
+            if (list.Count == 0)
+            {
+                Console.WriteLine("The Json file is empty");
+            }
+            else
+            {
+                foreach (Member member in list)
+                {
+                    listOfMembers.Add(member);
+                }
+            }
+        }
+        private void NewMemberJoin()
         {
             Client.GuildMemberAdded += Client_GuildMemberAdded;
         }
-
-        private Task Client_GuildMemberAdded(GuildMemberAddEventArgs e)
+        private async Task Client_GuildMemberAdded(GuildMemberAddEventArgs e)
         {
-            throw new NotImplementedException();
+
+            DiscordRole role = e.Guild.Roles.FirstOrDefault(role => role.Name == "Member");
+            await e.Member.GrantRoleAsync(role,"New member");
+            j.SaveOneMember(new Member(e.Member.Username,e.Member.Id,e.Member.Roles));
         }
 
         private void GetAllMembers()
@@ -89,33 +157,34 @@ namespace Ark_DiscordBot
                 {
                     listOfMembers.Add(new Member(dMember.DisplayName, dMember.Id, dMember.Roles));
                 }
+                j.SaveMembers(listOfMembers);
             }
             else
             {
                 foreach (DiscordMember dMember in e.Guild.Members)
                 {
-                    foreach (Member localMember in listOfMembers)
+                    if (CheckForNewUser(dMember.Id) == false)
                     {
-                        if (dMember.Id != localMember.MemberID)
-                        {
-                            listOfMembers.Add(new Member(dMember.DisplayName, dMember.Id, dMember.Roles));
-                            foreach (var item in listOfMembers)
-                            {
-                                Console.WriteLine(item.Name + item.MemberID + item.Roles);
-                            }
-                        }
+                        listOfMembers.Add(new Member(dMember.DisplayName, dMember.Id, dMember.Roles));
                     }
                 }
             }
             return Task.CompletedTask;
         }
-
+        
+        private bool CheckForNewUser(ulong id)
+        {
+            for (int i = 0; i < listOfMembers.Count; i++)
+            {
+                if (listOfMembers[i].MemberID == id)
+                {
+                    return !!true;
+                }
+            }
+            return !!false;
+        }
         private Task OnStart(ReadyEventArgs e)
         {
-            foreach (DiscordGuild guild in e.Client.Guilds.Values)
-            {
-                guild.Roles
-            }
             return Task.CompletedTask;
         }
     }
---
+++ Ark-DiscordBot/Json.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Ark_DiscordBot
+{
+    class Json
+    {
+        private string path = "Members.json";
+        public void UpdateMembers(List<Member> members)
+        {
+            string json = JsonConvert.SerializeObject(members.ToArray());
+            File.WriteAllText(path, json);
+        }
+        public void SaveMembers(List<Member> members)
+        {
+            string json = JsonConvert.SerializeObject(members.ToArray());
+            File.WriteAllText(path,json);
+            
+        }
+        public void SaveOneMember(Member member)
+        {
+            List<Member> temp = new List<Member>();
+            temp = ReadMembers();
+            temp.Add(member);
+            string json = JsonConvert.SerializeObject(temp.ToArray());
+            File.WriteAllText(path, json);
+
+        }
+        public List<Member> ReadMembers()
+        {
+            string jString = File.ReadAllText(path);
+            if (jString == string.Empty)
+            {
+                return new List<Member>();
+            }
+            List<Member> members = JsonConvert.DeserializeObject<List<Member>>(jString);
+            return members;
+        }
+    }
+}
